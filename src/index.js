3e6ab2f348585757ee75aca5fde485ce418c3fb3
Janne Jäppinen <janne.jappinen@protonmail.com>
2018-04-01 17:02:16 +0300

    Babel config. Nodemon for dev. Basic functionality.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-const Discord = require('discord.js');
+import Discord from 'discord.js';
+import config from './config';
+
+
 const client = new Discord.Client();
 
-// client.on('ready', () => {
-//   console.log(`Logged in as ${client.user.tag}!`);
-// });
-//
-console.log('TROLOLOL');
-// client.on('message', msg => {
-//   if (msg.content === 'ping') {
-//     msg.reply('pong');
-//   }
-// });
-//
-// client.login('token');
+client.login(config.token);
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
