4f673d9988c260d652ce2a06bd6ff2266cd2edb8
Janne Jäppinen <janne.jappinen@protonmail.com>
2019-02-03 21:51:20 +0200

    Run prettier and add todos

--- src/hs.js
+++ src/hs.js
@@ -1,39 +1,57 @@
-import axios from 'axios';
-import config from './config';
+import axios from "axios";
+import config from "./config";
 
+// TODO: async await
 
-const hs = (msg) => {
-  const searchQuery = msg.content.replace('?hs ', '');
+const hs = msg => {
+  const searchQuery = msg.content.replace("?hs ", "");
 
-  axios.get(`https://omgvamp-hearthstone-v1.p.mashape.com/cards/${searchQuery}`, { headers: { 'X-Mashape-Key': config.mashapeKey } })
+  axios
+    .get(`${config.hs_api_url}${searchQuery}`, {
+      headers: { "X-Mashape-Key": config.mashapeKey }
+    })
     .then(res =>
-      res.data.forEach((card) => {
-        if (card.type !== 'Enchantment' && !(card.type === 'Hero' && !card.cost)) {
+      res.data.forEach(card => {
+        if (
+          card.type !== "Enchantment" &&
+          !(card.type === "Hero" && !card.cost)
+        ) {
           msg.reply(card.img);
           let reply;
-          if (card.type === 'Minion') {
-            reply = `**${card.name}** - ${card.cost} mana ${card.attack}/${card.health} ${card.race ? card.race : ''} ${card.text ? `- ${card.text}` : ''}`;
-          } else if (card.type === 'Spell') {
+          if (card.type === "Minion") {
+            reply = `**${card.name}** - ${card.cost} mana ${card.attack}/${
+              card.health
+            } ${card.race ? card.race : ""} ${
+              card.text ? `- ${card.text}` : ""
+            }`;
+          } else if (card.type === "Spell") {
             reply = `**${card.name}** - ${card.cost} mana spell - ${card.text}`;
-          } else if (card.type === 'Hero') {
-            reply = `**${card.name}** - ${card.cost} mana - ${card.armor} armor - ${card.text}`;
-          } else if (card.type === 'Weapon') {
-            reply = `**${card.name}** - ${card.cost} mana ${card.attack}/${card.durability} ${card.type} ${card.text ? `- ${card.text}` : ''}`;
-          } else if (card.type === 'Hero Power') {
+          } else if (card.type === "Hero") {
+            reply = `**${card.name}** - ${card.cost} mana - ${
+              card.armor
+            } armor - ${card.text}`;
+          } else if (card.type === "Weapon") {
+            reply = `**${card.name}** - ${card.cost} mana ${card.attack}/${
+              card.durability
+            } ${card.type} ${card.text ? `- ${card.text}` : ""}`;
+          } else if (card.type === "Hero Power") {
             reply = `**${card.name}** - ${card.cost} mana - ${card.text}`;
           }
-          msg.reply(reply
-            .replace(/<b>/gi, '**')
-            .replace(/<\/b>/gi, '**')
-            .replace(/<i>/gi, '*')
-            .replace(/<\/i>/gi, '*')
-            .replace(/_/gi, ' ')
-            .replace(/\\n/gi, ' ')
-            .replace(/\$/gi, '')
-            .replace(/\[x]/gi, ''));
+          msg.reply(
+            reply
+              .replace(/<b>/gi, "**")
+              .replace(/<\/b>/gi, "**")
+              .replace(/<i>/gi, "*")
+              .replace(/<\/i>/gi, "*")
+              .replace(/_/gi, " ")
+              .replace(/\\n/gi, " ")
+              .replace(/\$/gi, "")
+              .replace(/\[x]/gi, "")
+          );
         }
-      }))
-    .catch(() => msg.reply('Card not found.'));
+      })
+    )
+    .catch(() => msg.reply("Card not found."));
 };
 
 export default hs;
