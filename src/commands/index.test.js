e0abaa31e585fd99c767d0fe962362cf2809de08
Janne Jäppinen <janne.jappinen@protonmail.com>
2021-10-01 21:21:14 +0300

    Default command for each module. Allow same alias to be used in different modules (#24)
    Co-authored-by: Janne Jäppinen <Janchu@users.noreply.github.com>

--- src/commands/index.test.js
+++ src/commands/index.test.js
@@ -1,14 +1,37 @@
-import commands from '.';
+import { availableCommandNamesAndAliases } from '.';
 
 describe('Commands / Common', () => {
+  test('No duplicate module names', () => {
+    const moduleNames = Object.keys(availableCommandNamesAndAliases).map(
+      (key) => key,
+    );
+    expect(new Set(moduleNames).size === moduleNames.length).toBe(true);
+  });
   test('No duplicate command names', () => {
-    const commandNames = Object.values(commands).map((command) => command.name);
+    const commandNames = Object.keys(availableCommandNamesAndAliases)
+      .map((key) => {
+        const { commands } = availableCommandNamesAndAliases[key];
+        return commands.map(({ name }) => name);
+      })
+      .flat();
     expect(new Set(commandNames).size === commandNames.length).toBe(true);
   });
-  test('No duplicate command name aliases', () => {
-    const commandAliases = Object.values(commands)
-      .map((command) => command.aliases)
-      .flat();
-    expect(new Set(commandAliases).size === commandAliases.length).toBe(true);
+  test('No duplicate command names or aliases within each module', () => {
+    Object.keys(availableCommandNamesAndAliases).forEach((key) => {
+      const { commands } = availableCommandNamesAndAliases[key];
+      const moduleNamesAndAlieses = [
+        ...commands.map(({ name }) => name),
+        ...commands.map(({ aliases }) => aliases),
+      ].flat();
+      expect(
+        new Set(moduleNamesAndAlieses).size === moduleNamesAndAlieses.length,
+      ).toBe(true);
+    });
+  });
+  test('Each module has a default command', () => {
+    Object.keys(availableCommandNamesAndAliases).forEach((key) => {
+      const { defaultCommand } = availableCommandNamesAndAliases[key];
+      expect(!!defaultCommand).toBe(true);
+    });
   });
 });
