e2c6807718dc2bed0036bb9cac91d40d99e37fb7
Janne Jäppinen <janne.jappinen@protonmail.com>
2022-10-13 01:57:08 +0300

    Remove outdated tests and mocks

--- src/commands/index.test.js
+++ src/commands/index.test.js
@@ -1,37 +1,8 @@
-import { availableCommandNamesAndAliases } from ".";
+import commands from ".";
 
 describe("Commands / Common", () => {
-  test("No duplicate module names", () => {
-    const moduleNames = Object.keys(availableCommandNamesAndAliases).map(
-      (key) => key
-    );
-    expect(new Set(moduleNames).size === moduleNames.length).toBe(true);
-  });
   test("No duplicate command names", () => {
-    const commandNames = Object.keys(availableCommandNamesAndAliases)
-      .map((key) => {
-        const { commands } = availableCommandNamesAndAliases[key];
-        return commands.map(({ name }) => name);
-      })
-      .flat();
+    const commandNames = commands.map((command) => command.data.name);
     expect(new Set(commandNames).size === commandNames.length).toBe(true);
-  });
-  test("No duplicate command names or aliases within each module", () => {
-    Object.keys(availableCommandNamesAndAliases).forEach((key) => {
-      const { commands } = availableCommandNamesAndAliases[key];
-      const moduleNamesAndAlieses = [
-        ...commands.map(({ name }) => name),
-        ...commands.map(({ aliases }) => aliases),
-      ].flat();
-      expect(
-        new Set(moduleNamesAndAlieses).size === moduleNamesAndAlieses.length
-      ).toBe(true);
-    });
-  });
-  test("Each module has a default command", () => {
-    Object.keys(availableCommandNamesAndAliases).forEach((key) => {
-      const { defaultCommand } = availableCommandNamesAndAliases[key];
-      expect(!!defaultCommand).toBe(true);
-    });
   });
 });
